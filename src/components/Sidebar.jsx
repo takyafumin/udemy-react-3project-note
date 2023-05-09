4a099b2e436f37eb136ebd1ae55970fde32fbe11
takyafumin <takyafumin@gmail.com>
2023-05-09 12:16:35 +0900

    Sidebar実装

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容です。</p>
+          <small>最後の修正日:xx/xx</small>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
-
+export default Sidebar;
