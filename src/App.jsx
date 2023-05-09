93f2d602a68dbbb0c91e79f28f8a95db120f8812
takyafumin <takyafumin@gmail.com>
2023-05-09 11:59:28 +0900

    SidebarとMainコンポーネントひな形作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
-      <div className="App"></div>
-    </>
-  )
+    <div className="App">
+      <Sidebar></Sidebar>
+      <Main></Main>
+    </div>
+  );
 }
 
-export default App
+export default App;
